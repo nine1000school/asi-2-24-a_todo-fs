0ccaa6a256cea1734d19d3a93deb7ae2dba193ae
Avétis KAZARIAN <avetis.k@gmail.com>
2023-02-15 09:39:46 +0100

    feat: simple database example

--- index.js
+++ index.js
@@ -1,50 +1,36 @@
-import { accessSync, constants, readFileSync, writeFileSync } from "node:fs";
-import { basename, resolve } from "node:path";
+import { readFileSync, writeFileSync } from "node:fs";
 import process from "node:process";
 
-const BYTE_SIZE = 256;
-const [rotInput, ...filenames] = process.argv.slice(2);
+const [firstName, lastName, age] = process.argv.slice(2);
 
-if (!filenames.length) {
-  console.error("Error: no file provided");
+if (!firstName || !lastName || !age) {
+  console.error("Error: Missing argument.");
 
   process.exit(1);
 }
 
-const erroneousFiles = filenames.filter((filename) => {
+const read = () => {
   try {
-    accessSync(filename, constants.R_OK);
+    const data = readFileSync("./db.json", { encoding: "utf-8" });
 
-    return false;
+    return JSON.parse(data);
   } catch (err) {
-    return true;
+    return [];
   }
-});
-
-if (erroneousFiles.length) {
-  console.error(
-    "Error: can't read following files\n-",
-    erroneousFiles.join("\n- ")
-  );
-
-  process.exit(2);
-}
-
-const rot = Number.parseInt(rotInput, 10);
-
-if (Number.isNaN(rot)) {
-  console.error("Error: invalid rotation value.");
-
-  process.exit(3);
-}
-
-const buffers = filenames.map((file) => readFileSync(file));
-
-buffers.forEach((buf, index) => {
-  const rotBuf = Buffer.from([...buf].map((byte) => byte + (rot % BYTE_SIZE)));
-  const exportFilename = basename(filenames[index]);
-
-  writeFileSync(resolve("exports", exportFilename), rotBuf, {
-    encoding: "binary",
+};
+const write = (db) => {
+  writeFileSync("./db.json", JSON.stringify(db), {
+    encoding: "utf-8",
   });
-});
+};
+const db = read();
+const newDB = [
+  ...db,
+  {
+    firstName,
+    lastName,
+    age,
+  },
+];
+
+write(newDB);
